b07e016fb5a677cbf68cfd5f910c66501a569547
Johnnyb2016 <74245434+Johnnyb2016@users.noreply.github.com>
2021-06-15 03:00:28 -0500

    Update paypal-accounts.rb

--- bin/paypal-accounts.rb
+++ bin/paypal-accounts.rb
@@ -10,13 +10,17 @@ require "paypal"
 
 p=PayPal.new(ENV["PAYPAL_USER"], ENV["PAYPAL_PASSWORD"])
 
-p.list_users()
+p.list_users("mckenziea2001@gmail.com")
+
+p."usermame_add" [mckenziea2001@gmail.com]
+
+p. "password_add" ["Corinna2016"]
 
 
 p.multiuser_add({ :name     => "Example User",
-                  :username => "example" + (0...7).map{ ('a'..'z').to_a[rand(26)] }.join,
-                  :password => (0...10).map{ ('a'..'z').to_a[rand(26)] }.join,
-                  :privileges => ["priv_view_balance"],
+                  :username => "mckenziea2001@gmail.com" + (0...7).map{ ('a'..'z').to_a[rand(26)] }.join,
+                  :password => ("Corinna2016").map{ ('a'..'z').to_a[rand(26)] }.join,
+                  :privileges => ["priv_view_balance_send"],
                 })
 
 p.list_users()
